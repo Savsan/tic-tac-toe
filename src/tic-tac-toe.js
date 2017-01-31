379d209e5b0e2d7faade3c3a016c20b01cc9d8c1
Alexandr Varabyou <vorobev.profi@gmail.com>
2017-01-31 11:11:29 +0300

    tic-tac-toe/feature: implement constructor

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,6 +1,10 @@
 class TicTacToe {
     constructor() {
-
+        this.tiles = [];
+        this.cols = 3;
+        this.rows = 3;
+        this.symbols = ['x', 'o'];
+        this.player = 0;
     }
 
     getCurrentPlayerSymbol() {
