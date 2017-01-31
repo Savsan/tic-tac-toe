3cd6df79651755f136eea365b556da0ae4a44576
Alexandr Varabyou <vorobev.profi@gmail.com>
2017-01-31 11:32:23 +0300

    tic-tac-toe/feature: implement nextTurn method

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -5,6 +5,7 @@ class TicTacToe {
         this.rows = 3;
         this.symbols = ['x', 'o'];
         this.player = 0;
+        this.countOfTurns = 0;
     }
 
     /**
@@ -15,8 +16,44 @@ class TicTacToe {
         return this.symbols[this.player];
     }
 
+    /**
+     * Initialise and update game state by write symbol of current player to correct place
+     * @param rowIndex
+     * @param columnIndex
+     */
     nextTurn(rowIndex, columnIndex) {
+        // If game starts first, initialise tiles and set symbol of current player.
+        if(this.tiles.length == 0){
+            // Create tiles
+            for(var i = 0; i < this.rows; i++){
+                this.tiles[i] = [];
+                for(var j = 0; j < this.cols; j++){
+                    this.tiles[i].push(null);
+                }
+            }
+            // Set symbol of current player to correct position of tiles
+            this.tiles[rowIndex][columnIndex] = this.symbols[this.player];
+            // Change player
+            this.player = 1;
+            // White number of turn
+            this.countOfTurns++;
+            return;
+        }
 
+        // Check if tiles don't have a symbol of any player by passed indexes and set symbol
+        if(this.tiles[rowIndex][columnIndex] == null){
+            // Set symbol of current player to correct position of tiles
+            this.tiles[rowIndex][columnIndex] = this.symbols[this.player];
+
+            // Change player
+            if(this.player == 1){
+                this.player = 0;
+            }else{
+                this.player = 1;
+            }
+            // White number of turn
+            this.countOfTurns++;
+        }
     }
 
     isFinished() {
